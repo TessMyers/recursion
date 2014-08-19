fa9be8b6fd9617b594b801631ea805293e28effe
Tess Myers <tess@tessmyers.com>
2014-08-18 17:46:40 -0700

    stringifiedJSON implemented

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -1,7 +1,28 @@
-// this is what you would do if you liked things to be easy:
-// var stringifyJSON = JSON.stringify;
 
-// but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
-  // your code goes here
+
+  if (typeof obj === 'number' || obj === null || typeof obj === 'boolean') {
+    return '' + obj
+  } else if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  } else if (typeof obj === 'undefined' || typeof obj === 'function') {
+    return '{}'
+  } else if ({}.toString.call(obj) === "[object Array]") {
+    var stringified = [];
+    for (var i = 0; i < obj.length; i++) {
+      stringified.push(stringifyJSON(obj[i]));
+    }
+    return '[' + stringified.join(',') + ']'
+  }
+
+  var stringified = [];
+  for (key in obj) {
+    if (typeof obj[key] != 'undefined' && typeof obj[key] != 'function'){
+      var stringKeyValue = stringifyJSON(key) + ':' + stringifyJSON(obj[key]);
+      stringified.push(stringKeyValue);
+    }
+  }
+   return '{' + stringified.join(',') + '}';
 };
+
+// I could probably stand to smooth the flow a bit, especially when handling functions and undefined values.
