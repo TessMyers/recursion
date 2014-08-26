8afe8398856431c7cc08075c49a8229b35e9446d
Tess Myers <tess@tessmyers.com>
2014-08-25 22:19:23 -0700

    first two tests on getElements... working

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -1,9 +1,19 @@
-// If life was easy, we could just do things the easy way:
-// var getElementsByClassName = function (className) {
-//   return document.getElementsByClassName(className);
-// };
 
-// But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className){
-  // your code here
+  console.log('begin')
+  console.log(document.body)
+  var arr = [];
+  arr.push(document.body);
+
+  for (var i = 0; i < document.body.childNodes.length; i++) {
+    var name = document.body.childNodes[i].className
+    if (typeof name != 'undefined' && name.indexOf(className) >= 0) {
+      console.log('pushing in new node')
+      arr.push(document.body.childNodes[i])
+    }
+  }
+
+  console.log('is', arr)
+  console.log('should be:', document.getElementsByClassName('targetClassName'))
+  return arr;
 };
